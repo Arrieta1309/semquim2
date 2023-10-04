1f7d55fca0739a39698bc171636ce7c333d2bebc
Arrieta1309 <146982456+Arrieta1309@users.noreply.github.com>
2023-10-04 11:38:36 -0600

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.ndimage.interpolation import zoom
 from streamlit_drawable_canvas import st_canvas
 from utils import process_image
-st.markdown("# la :blue[mejor] :green[aplicacion del curso] :pencil: 💻🤖")
+st.markdown("# la :blue[mejor] :green[aplicacion_del_curso] :pencil: 💻🤖")
 
 # Load trained model
 model = tf.keras.models.load_model('mi_modelo.h5')
